7addad3556b9ab5a57e25afbd40c449e0649ff86
Akash Bhave <akashbbhave@gmail.com>
2019-05-24 21:51:06 -0400

    Add player information to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
-    <div id="header" class="text-light bg-tertiary flex items-center h-12 px-2">
-        <h1>毕业</h1>
+    <div id="header" class="text-light bg-tertiary flex items-center justify-between h-12 px-2">
+        <p>毕业</p>
+        <div v-if="$store.state.name">
+            <span
+                :style="'color: ' + $store.state.character.color"
+            >{{ $store.state.character.chineseName }}</span>
+            <span>&nbsp;/&nbsp;</span>
+            <span>{{ $store.state.name }}</span>
+        </div>
     </div>
 </template>
 
