76d96882a437966061005529ebb386ed124c0ee8
Akash Bhave <akashbbhave@gmail.com>
2019-05-27 11:10:05 -0400

    Improve responsiveness of intro page:

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <div class="my-8">
-            <img class="h-64 mx-auto" src="@/assets/img/logo/logo.png" alt>
+            <img class="sm:h-64 mx-auto" src="@/assets/img/logo/logo.png" alt>
             <h1 class="text-5xl font-bold">B&igrave; Y&egrave;</h1>
             <p>A choose-your-own-adventure Chinese review game.</p>
         </div>
-        <div class="my-8">
+        <div class="my-6">
             <button
                 @click="navigateStart()"
-                class="btn btn-primary text-light px-6 py-3 mx-4 text-3xl"
+                class="btn btn-primary text-light px-6 py-3 mx-4 my-4 text-3xl"
             >开始</button>
             <button
                 @click="navigateCredits()"
